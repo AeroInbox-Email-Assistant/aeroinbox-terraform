43a07e183bd091959331dcdea95fbcd16fea17b1
Akash <muvvalaakash0421@gmail.com>
2026-06-21 20:55:51 +0530

    backend

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-aeroinbox-terraform-state"
-    storage_account_name = "staeroinboxtfstate"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-    use_oidc             = true
-  }
+  backend "azurerm" {}
 }
